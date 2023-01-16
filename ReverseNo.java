8cd7c28ce0c56501f7fecbe8f0fa671e2fc95061
ritanshbagal <ritanshbagal@gmail.com>
2023-01-17 01:29:04 +0530

    Reverse The Number

---
+++ ReverseNo.java
@@ -0,0 +1,13 @@
+public class ReverseNo {
+    public static void main(String[] args) {
+        int n = 10899;
+        int rev = 0;
+
+        while(n>0){
+            int lastDigit = n%10;
+            rev = (rev*10) + lastDigit;
+            n/=10;
+        }
+        System.out.println(rev);
+    }
+}
