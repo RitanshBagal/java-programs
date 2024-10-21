68ee1e2fd7585dee3545b1425dd7559e322b351f
ritanshbagal <ritanshbagal@gmail.com>
2024-10-21 13:33:36 +0530

    kth Ancestor

---
+++ kthAncestor.java
@@ -0,0 +1,44 @@
+public class kthAncestor {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public static int kthAncestor(Node root, int n, int k){
+        if(root == null){
+            return -1;
+        }
+        if(root.data == n){
+            return 0;
+        }
+        int leftDist = kthAncestor(root.left, n, k);
+        int rightDist = kthAncestor(root.right, n, k);
+
+        if(leftDist == -1 && rightDist == -1){
+            return -1;
+        }
+        int max = Math.max(leftDist, rightDist);
+
+        if(max+1 == k){
+            System.out.println(root.data);
+        }
+
+        return max+1;
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        kthAncestor(root,5,2);
+    }
+}
